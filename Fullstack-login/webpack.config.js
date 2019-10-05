2546d8764341b82903c41482fad51953e92b0845
Sarju Mulmi <mulmi.sarju@gmail.com>
2019-10-05 06:48:47 -0400

    webpack config customized

---
+++ Fullstack-login/webpack.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+const path = require('path')
+
+const config = {
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+    proxy: { "/api/**": { target: 'http://localhost:3003', secure: false }  }
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+    ]
+  }
+}
+module.exports = config
